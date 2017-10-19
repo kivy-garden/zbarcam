d9280b4813d95429976b11ce3441731785ed4ba5
Andre Miras <andre.miras@gmail.com>
2017-10-19 21:03:21 +0200

    Compile only if required

--- python-for-android/recipes/libiconv/__init__.py
+++ python-for-android/recipes/libiconv/__init__.py
@@ -1,3 +1,4 @@
+import os
 from pythonforandroid.toolchain import shprint, current_directory
 from pythonforandroid.recipe import Recipe
 from multiprocessing import cpu_count
@@ -11,6 +12,10 @@ class LibIconvRecipe(Recipe):
     url = 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-{version}.tar.gz'
 
     patches = ['libiconv-1.15-no-gets.patch']
+
+    def should_build(self, arch):
+        return not os.path.exists(
+                os.path.join(self.ctx.get_libs_dir(arch.arch), 'libiconv.so'))
 
     def build_arch(self, arch):
         super(LibIconvRecipe, self).build_arch(arch)
