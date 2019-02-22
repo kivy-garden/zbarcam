ae0bc3008ceecc50036a897b3f3fdaf8b829dd58
Andre Miras <andre.miras@gmail.com>
2019-02-22 21:15:46 +0100

    Covering more test cases
    Also fixes tox requirements.

--- tests/test_zbarcam.py
+++ tests/test_zbarcam.py
@@ -19,6 +19,16 @@ class TestZBarCam(unittest.TestCase):
         with mock.patch('kivy.uix.anchorlayout.AnchorLayout.__init__'):
             self.zbarcam = ZBarCam()
 
+    def test_detect_qrcode_frame_no_qrcode(self):
+        """
+        Checks `_detect_qrcode_frame()` returns empty list on no qrcode.
+        """
+        fixture_path = os.path.join(FIXTURE_DIR, 'no_qr_code.png')
+        texture = Image(fixture_path).texture
+        code_types = self.zbarcam.code_types
+        symbols = self.zbarcam._detect_qrcode_frame(texture, code_types)
+        self.assertEqual(symbols, [])
+
     def test_detect_qrcode_frame_one_qrcode(self):
         """
         Checks `_detect_qrcode_frame()` can detect one qrcode.
@@ -30,3 +40,31 @@ class TestZBarCam(unittest.TestCase):
         self.assertEqual(
             symbols,
             [ZBarCam.Symbol(type='QRCODE', data='zbarlight test qr code')])
+
+    def test_detect_qrcode_frame_one_qrcode_one_ean(self):
+        """
+        Checks `_detect_qrcode_frame()` can detect one qrcode and one ean.
+        """
+        fixture_path = os.path.join(FIXTURE_DIR, 'one_qr_code_and_one_ean.png')
+        texture = Image(fixture_path).texture
+        code_types = self.zbarcam.code_types
+        symbols = self.zbarcam._detect_qrcode_frame(texture, code_types)
+        # currently detects no codes, but that's a bug
+        self.assertEqual(symbols, [])
+
+    def test_detect_qrcode_frame_two_qrcodes(self):
+        """
+        Checks `_detect_qrcode_frame()` can detect two qrcodes.
+        """
+        fixture_path = os.path.join(FIXTURE_DIR, 'two_qr_codes.png')
+        texture = Image(fixture_path).texture
+        code_types = self.zbarcam.code_types
+        symbols = self.zbarcam._detect_qrcode_frame(texture, code_types)
+        self.assertEqual(
+            symbols, [
+                ZBarCam.Symbol(
+                    type='QRCODE', data='second zbarlight test qr code'),
+                ZBarCam.Symbol(
+                    type='QRCODE', data='zbarlight test qr code'),
+            ]
+        )
