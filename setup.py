e637b8c0abd7c9048dd90cf9567122932ab0544b
Andre Miras <andre.miras@gmail.com>
2019-08-31 01:07:00 +0200

    Updates setup.py
    - migrates to setuptools
    - adds long description
    - single source of truth on version

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
+from kivy_garden.zbarcam import version
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(name='zbarcam',
-      version='20171220',
+      version=version.__version__,
       description='Real time Barcode and QR Code scanner Edit',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
       author='Andre Miras',
       url='https://github.com/AndreMiras/garden.zbarcam',
       packages=['kivy_garden.zbarcam'],
