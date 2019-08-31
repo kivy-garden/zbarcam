9ab5732c151738253322ac81739de79e741fa87d
Andre Miras <andre.miras@gmail.com>
2019-09-01 00:36:44 +0200

    docs sphinx_rtd_theme, project name and author

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,9 +17,9 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Flower'
-copyright = '2019, Kivy'
-author = 'Kivy'
+project = 'Zbarcam'
+copyright = '2019, Andre Miras'
+author = 'Andre Miras'
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
