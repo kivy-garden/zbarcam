bf26bb2636f9e4a0eec465370d44878537113912
Andre Miras <andre.miras@gmail.com>
2019-09-11 00:13:35 +0200

    2019.0910

--- src/kivy_garden/zbarcam/version.py
+++ src/kivy_garden/zbarcam/version.py
@@ -1,7 +1,7 @@
-__version__ = '2019.0909'
+__version__ = '2019.0910'
 # The `__version_code__` is used for the F-Droid auto update and should match
 # the `versionCode` from the `build.gradle` file located in:
 # `.buildozer/android/platform/build/dists/zbarcamdemo/`
 # The auto update method used is the `HTTP`, see:
 # https://f-droid.org/en/docs/Build_Metadata_Reference/#UpdateCheckMode
-__version_code__ = 721202809
+__version_code__ = 721202810
